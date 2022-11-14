626eae547fd00fa1a7892894728346b0ae670e6c
Karol <93782725+petitess@users.noreply.github.com>
2022-11-14 14:58:17 +0100

    Add files via upload

---
+++ Apps/AppInfo/7z-x64.ps1
@@ -0,0 +1,29 @@
+﻿Param(
+[Parameter(Mandatory=$true)]
+[ValidateSet("Install", "Uninstall")]
+[String[]]
+$Mode
+)
+
+## To install:
+## powershell.exe -ExecutionPolicy Bypass -file Install.ps1 -Mode Install 
+
+## To uninstall:
+## powershell.exe -ExecutionPolicy Bypass -file Install.ps1 -Mode Uninstall 
+
+
+If ($Mode -eq "Install")
+ 
+{
+# Installing...
+Start-Process msiexec.exe -Wait -ArgumentList '/i 7z-x64.msi /qn /norestart ACCEPT_EULA=YES'
+ 
+}
+ 
+If ($Mode -eq "Uninstall")
+ 
+{
+ # Uninstalling...
+Start-Process msiexec.exe -Wait -ArgumentList '/x 7z-x64.msi /qn /norestart'
+ 
+}
