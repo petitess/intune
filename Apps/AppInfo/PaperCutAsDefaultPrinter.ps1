428263dc2d067d24c59f34518fa6819cf84ff5d8
Karol <93782725+petitess@users.noreply.github.com>
2023-02-20 13:52:51 +0100

    Set default printer

---
+++ Apps/AppInfo/PaperCutAsDefaultPrinter.ps1
@@ -0,0 +1,34 @@
+﻿Param(
+[Parameter(Mandatory=$true)]
+[ValidateSet("Install", "Uninstall")]
+[String[]]
+$Mode
+)
+
+## För att installera:
+## powershell.exe -ExecutionPolicy Bypass -file Install.ps1 -Mode Install 
+
+## För att avinstallera:
+## powershell.exe -ExecutionPolicy Bypass -file Install.ps1 -Mode Uninstall 
+
+#Detection method: File or folder exists - PaperCut.txt
+
+$fileToDetect = "PaperCut.txt"
+
+If ($Mode -eq "Install")
+ 
+{
+$Printer = Get-CimInstance -Class Win32_Printer | where {$_.Name -match "PaperCut"}
+Invoke-CimMethod -InputObject $Printer -MethodName SetDefaultPrinter 
+
+New-Item -Path "C:\Users" -Name $fileToDetect -ItemType File `
+-Value "PaperCut Printer as default printer. Den filen skapades av Intune." `
+-Confirm:$false
+}
+ 
+If ($Mode -eq "Uninstall")
+ 
+{
+ Set-Itemproperty -path 'HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows' -Name 'LegacyDefaultPrinterMode' -value '0'
+ Remove-Item -Path "C:\Users\$fileToDetect"
+}
